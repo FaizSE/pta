f728a1d8d11a5a6b96f85b6562af497bcee74df5
FaizSE <32590780+FaizSE@users.noreply.github.com>
2018-03-14 16:04:35 +0800

    Data Info v6

--- uploadcsv/views.py
+++ uploadcsv/views.py
@@ -4,6 +4,8 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.conf import settings
 import pandas as pd
+import io
+
 
 
 def viewcsv(request, pk):
@@ -14,9 +16,25 @@ def opencsv(request, pk):
     filelocation = str(get_object_or_404(File, pk=pk).filelocation)
     csvfile=settings.MEDIA_ROOT + '/' + filelocation
     data = pd.read_csv(csvfile, encoding = "ISO-8859-1")
+
+    def process_content_info(content: pd.DataFrame):
+        content_info = io.StringIO()
+        content.info(buf=content_info)
+        str_ = content_info.getvalue()
+
+        lines = str_.split("\n")
+        table = io.StringIO("\n".join(lines[3:-3]))
+        datatypes = pd.read_table(table, delim_whitespace=True, names=["column", "count", "null", "dtype"])
+        datatypes.set_index("column", inplace=True)
+        info = "\n".join(lines[0:2] + lines[-2:-1])
+        return info, datatypes
+
     data_html = data.to_html()
-    context = {'loaded_data': data_html}
+    data_info=process_content_info(data)
+    context = {'loaded_data': data_html, 'data_info':data_info}
     return render(request, 'opencsv.html', context)
+
+
 
 def newcsv(request):
     if request.method == 'POST':
