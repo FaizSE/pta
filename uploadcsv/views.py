f7adfa8b720ed6fe2b6991e7a9271f497a92aded
FaizSE <32590780+FaizSE@users.noreply.github.com>
2018-03-15 17:48:50 +0800

    Preprocess

--- uploadcsv/views.py
+++ uploadcsv/views.py
@@ -17,28 +17,14 @@ def opencsv(request, pk):
     csvfile=settings.MEDIA_ROOT + '/' + filelocation
     data = pd.read_csv(csvfile, encoding = "ISO-8859-1")
 
-    def process_content_info(content: pd.DataFrame):
-        content_info = io.StringIO()
-        content.info(buf=content_info)
-        str_ = content_info.getvalue()
+    if 'dropna' in request.POST:
+        data = data.dropna(how='all')
+        data.to_csv(csvfile, encoding="ISO-8859-1", index=False)
+    elif 'strip' in request.POST:
+        index = request.POST['index']
+        data[index]=data[index].str.strip()
+        data.to_csv(csvfile, encoding="ISO-8859-1",index=False)
 
-        lines = str_.split("\n")
-        table = io.StringIO("\n".join(lines[3:-3]))
-        datatypes = pd.read_table(table, delim_whitespace=True, names=["column", "count", "null", "dtype"])
-        datatypes.set_index("column", inplace=True)
-        info = '<br/>'.join(lines[0:2] + lines[-2:-1])
-        return info, datatypes
-
-    data_html = data.to_html()
-    data_info=process_content_info(data)
-    context = {'loaded_data': data_html, 'data_info':data_info}
-    return render(request, 'opencsv.html', context)
-
-def dropna(request):
-    filelocation = str(get_object_or_404(File, pk=7).filelocation)
-    csvfile=settings.MEDIA_ROOT + '/' + filelocation
-    data = pd.read_csv(csvfile, encoding = "ISO-8859-1")
-    data['Status Semasa'] = data['Status Semasa'].str.strip()
 
     def process_content_info(content: pd.DataFrame):
         content_info = io.StringIO()
@@ -53,8 +39,9 @@ def dropna(request):
         return info, datatypes
 
     data_html = data.to_html()
+    data_html=data_html.replace("\\r", "")
     data_info=process_content_info(data)
-    context = {'loaded_data': data_html, 'data_info':data_info}
+    context = {'loaded_data': data_html, 'data_info':data_info, 'pk':pk}
     return render(request, 'opencsv.html', context)
 
 def newcsv(request):
