c5f646949edaeb3db9a08f1c4459ea099a756e74
FaizSE <32590780+FaizSE@users.noreply.github.com>
2018-04-09 10:25:51 +0800

    Preprocess v9

--- preprocess/views.py
+++ preprocess/views.py
@@ -4,6 +4,8 @@ from django.conf import settings
 import pandas as pd
 import io
 import numpy as np
+from django.contrib import messages
+
 
 def viewcsv(request, pk):
     file = get_object_or_404(File, pk=pk)
@@ -19,8 +21,28 @@ def preprocesscsv(request, pk):
     colnametype=np.column_stack((colname, coltype))#Merge into 2D matrix
     pd.set_option('display.max_colwidth', -1)
 
+
+    def process_content_info(content: pd.DataFrame):#Get df.info() in HTML
+        content_info = io.StringIO()
+        content.info(buf=content_info)
+        str_ = content_info.getvalue()
+        lines = str_.split("\n")
+        table = io.StringIO("\n".join(lines[3:-3]))
+        datatypes = pd.read_table(table, delim_whitespace=True, names=["column", "count", "null", "dtype"])
+        datatypes.set_index("column", inplace=True)
+        info = '<br/>'.join(lines[0:2] + lines[-2:-1])
+        return info, datatypes
+
     def overwritedata():
         data.to_csv(csvfile, encoding="ISO-8859-1",index=False)
+        colname = list(data)
+        coltype = list(data.dtypes)  # Get list of headers type
+        colnametype = np.column_stack((colname, coltype))  # Merge into 2D matrix
+        data_html = data.to_html()
+        data_html = data_html.replace("\\r", "")
+        data_html = data_html.replace("\\n", "<br/>")
+        context = {'loaded_data': data_html, 'pk': pk, 'colname': colname, 'colnametype': colnametype}
+        return render(request, 'preprocess/opencsv.html', context)
 
     if 'dropna' in request.POST:#Remove empty row
         data = data.dropna(how='all')
@@ -52,29 +74,21 @@ def preprocesscsv(request, pk):
     elif 'changetype' in request.POST:#Change column type
         changetypecol = request.POST['changetypecol']
         changetypeto = request.POST['changetypeto']
+        try:
+            if changetypeto=='datetime64':
+                data[changetypecol] = pd.to_datetime(data[changetypecol])
+            else:
+                data[changetypecol] = data[changetypecol].astype(changetypeto)
+            coltype = list(data.dtypes)  # Get list of headers type
+            colnametype = np.column_stack((colname, coltype))  # Merge into 2D matrix
+            overwritedata()
 
-        if changetypeto=='datetime64':
-            data[changetypecol] = pd.to_datetime(data[changetypecol])
-        else:
-            data[changetypecol] = data[changetypecol].astype(changetypeto)
-        coltype = list(data.dtypes)  # Get list of headers type
-        colnametype = np.column_stack((colname, coltype))  # Merge into 2D matrix
-        overwritedata()
+        except:
+            messages.error(request, "Error, string exist in the column.")
 
-    def process_content_info(content: pd.DataFrame):#Get df.info() in HTML
-        content_info = io.StringIO()
-        content.info(buf=content_info)
-        str_ = content_info.getvalue()
-        lines = str_.split("\n")
-        table = io.StringIO("\n".join(lines[3:-3]))
-        datatypes = pd.read_table(table, delim_whitespace=True, names=["column", "count", "null", "dtype"])
-        datatypes.set_index("column", inplace=True)
-        info = '<br/>'.join(lines[0:2] + lines[-2:-1])
-        return info, datatypes
-
+    data_info=process_content_info(data)
     data_html = data.to_html()
     data_html=data_html.replace("\\r", "")
     data_html=data_html.replace("\\n", "<br/>")
-    data_info=process_content_info(data)
-    context = {'loaded_data': data_html, 'data_info':data_info, 'pk':pk, 'colname':colname, 'colnametype':colnametype}
+    context = {'loaded_data': data_html, 'pk':pk, 'colname':colname, 'colnametype':colnametype}
     return render(request, 'preprocess/opencsv.html', context)
